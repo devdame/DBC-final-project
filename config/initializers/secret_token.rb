efe2be1d568e4c457c715c2c7aa7a08e8b2aac44
estensland <estensland88@gmail.com>
2014-03-20 09:00:49 -0500

    empty rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CollegeCultureFit::Application.config.secret_key_base = 'd265226e8434997356dc18779daa4e189fc785cabe89690dfacff53aef580d90da4db3c0523f4791baa471cc057d1070b81d1e856297caffec2a0c5fabc2fad0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CollegeCultureFit::Application.config.session_store :cookie_store, key: '_college_culture_fit_session'

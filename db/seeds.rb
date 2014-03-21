17cffd24566700c1e4adec8a9deaa9d49a5bea29
thejessleigh <j.l.unrein@gmail.com>
2014-03-21 11:27:27 -0500

    bringin in the changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,13 +8,35 @@
 require 'httparty'
 require 'json'
 
-json = File.read('seeds/uofm32206_3_20_1342.json')
-parsed = JSON.parse(json)
+# json = File.read('seeds/uofm32206_3_20_1342.json')
+# parsed = JSON.parse(json)
 
-sample_data = []
+# sample_data = []
 
-parsed["items"].each do |item|
-  sample_data << item["title"]
+# parsed["items"].each do |item|
+#   sample_data << item["title"]
+# end
+
+# puts sample_data
+
+
+#############################
+Dir.foreach("/seeds") do |filename|
+  json = File.read(filename)
+  feed_id = filename.gsub(/(\d+)[a-z].+/i, '\1')
+  parsed = JSON.parse(json)
+
+  parsed["items"].each do |item|
+    original_post.create(text: item["title"], publish_date: item["publishDate"], feed_id: feed_id)
+  end
 end
 
-puts sample_data
+
+
+
+
+# seeds file.each do |file|
+#   filename ~= /32206+/
+
+#   end
+# end

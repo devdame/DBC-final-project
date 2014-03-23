c106d6f0947484be0338d58f6afc41af697d84e9
nealcpeters <wisconsinpoet@gmail.com>
2014-03-22 21:12:14 -0500

    can properly parse csv files into the activerecord database

--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -1,6 +1,6 @@
 class Keyword < ActiveRecord::Base
   validates :analyzed_post_id, presence: true
-  validates :text, presence: true, uniqueness: true
+  validates :text, presence: true
   validates :sentiment, presence: true
   validates :confidence, presence: true
 
--- app/models/reference_word.rb
+++ app/models/reference_word.rb
@@ -1,6 +1,6 @@
 class ReferenceWord < ActiveRecord::Base
   validates :topic_id, presence: true
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
 
   belongs_to :topic
 end

5e2098d25f73b7342c8f7dcb8854db948d38b567
thejessleigh <j.l.unrein@gmail.com>
2014-03-25 17:45:15 -0500

    have word names for reference words within a topic.

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -6,6 +6,18 @@ class TopicsController < ApplicationController
 
   def show
     @topic = Topic.find(params[:id])
+
+
+    ##########################################
+
+    #Most Popular Words in Topic
+    # holding = {"popular_words"}
+
+    reference_words = @topic.reference_words.all
+    @popular_words = []
+    reference_words.each do |word|
+      @popular_words << SchoolWordCount.find_by(reference_word_id: word.id)
+    end
   end
 
 end

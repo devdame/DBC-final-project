27c99a50b2a4b5e1518508bdaca41dc73e8ad202
Andrew Wittrock <awittrock@gmail.com>
2014-03-25 14:45:00 -0500

    working on testworker

--- app/workers/TestWorker.rb
+++ app/workers/TestWorker.rb
@@ -7,4 +7,12 @@ class SomeWorker
   def perform
     AnalyzedPost.create(school_id: 1, overall_sentiment: "positive", original_publish_time: Time.now)
   end
+
+  def testcall
+  	geofeedia_id = 32206
+    url = "https://api.geofeedia.com/v1/search/geofeed/#{geofeedia_id}?format=json-default&appId=420880de&appKey=306ced14ef8ab2183b8264327c456806"
+    response = HTTParty.get(url)
+    parsed = JSON.parse(response)
+    parsed_items = parsed["items"]
+    post = OriginalPost.new(text: item["title"], original_publish_time: item["publishDate"], geofeedia_school_id: feed_id, school_id: school.id, external_id: item["externalId"])
 end

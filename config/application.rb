cf5ddfe0419329adb272b00838da64db31cc34be
Andrew Wittrock <awittrock@gmail.com>
2014-03-25 19:58:00 -0500

    setting assets precompile to false to try to remedy the heroku rejection

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,8 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
+config.assets.initialize_on_precompile = false
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)

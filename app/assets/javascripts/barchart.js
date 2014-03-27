7ac0b69c9c80922a9569ef901e2db049b7604b02
Daniel Lu <dandydanny@gmail.com>
2014-03-27 18:36:44 -0500

    Revert overwritten barchart js

--- app/assets/javascripts/barchart.js
+++ app/assets/javascripts/barchart.js
@@ -1,16 +1,16 @@
+// used by single topic view (topic show)
 function barChart(data) {
 
-var width = 420,
+var width = 800,
     barHeight = 30;
 
-    var w = 420,                        //width
+    var w = 800,                        //width
         h = 30*data.length,                       //height
         color = d3.scale.category20c(),     //builtin range of colors
-        x = d3.scale.sqrt()
-          .range([0, width]);
+        scaler = d3.scale.linear()
+          .range([0, w-250]);
 
-  // var color = d3.scale.ordinal()
-  //     .range(["#3399FF", "#5DAEF8", "#86C3FA", "#ADD6FB", "#D6EBFD"]);
+
 
     var chart = d3.select("#bar-chart-topic-school")
         .append("svg:svg")              //create the SVG element inside the <body>
@@ -20,27 +20,47 @@ var width = 420,
             .attr("height", h)
         .append("svg:g")                //make a group to hold our bar
 
+    maxPosRange = d3.max(data, function(d) { return d.count });
+
     var bar = chart.selectAll("g")
         .data(data)
       .enter().append("g")
         .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; })
 
+    // bar bg
+    // bar.append("rect")
+    //   .attr("height", 0)
+    //   .attr("opacity", 0.2)
+    //   .transition()
+    //   .duration(800)
+    //   // .attr("x", 0)
+    //   .attr("opacity", 1)
+    //   .attr("width", w)
+    //   .attr("height", barHeight - 1)
+    //   .attr("fill", "rgba(100,100,100,0.3)" );
+
+    // actual data bars
     bar.append("rect")
       .attr("width", 0)
+      .attr("x", 250)
+      // .attr("x", 250)
       .transition()
-      .duration(800)
-      .attr("width", function(d) { return (d.count)*1.5;})
+      .duration(2000)
+      .attr("width", function(d) { return scaler(d.count/maxPosRange);})
       .attr("height", barHeight - 1)
       .attr("fill", function(d, i) { return color(i); } );
 
+
+    // text labels
     bar.append("text")
-    .attr("x", function(d) { return x(d) - 3; })
+    .attr("x", 240)       // fix this hard code offset in the future
     .attr("y", barHeight / 2)
     .attr("dy", ".35em")
-    .text(function(d) { return d.name; })
-    .attr("fill", "black")
+    .text(function(d) { return d.count; })
+    .attr("fill", "#606060")
     .attr("font-family", "sans-serif")
-    .attr("fill", "black")
-    .attr("font-family", "Sans-Serif");
+    .attr("font-size", "0.9em")
+    .attr("font-weight", "bold")
+    .attr("text-anchor", "end");
 
 }

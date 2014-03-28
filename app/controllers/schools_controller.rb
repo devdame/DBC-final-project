fd012777fa18aff8afeeda3e9e1927cc16f3ffda
Daniel Lu <dandydanny@gmail.com>
2014-03-27 19:20:59 -0500

    Disabled unused hash lookup in school controller

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -33,24 +33,24 @@ class SchoolsController < ApplicationController
     p @ratings_profile
     #Post count and sentiment by topic and most popular words by topic for a particular school
     ##########################################
-    school_data = {"school_topic_posts_count" => []}
+    # school_data = {"school_topic_posts_count" => []}
 
-    school_sorted_ratings = @ratings.order("total_post_count desc")
+    # school_sorted_ratings = @ratings.order("total_post_count desc")
 
-    @school.ratings.each_with_index do |rating, index|
-      school_data["school_topic_posts_count"] << {school: rating.school.name, name: rating.topic.name, count: rating.total_post_count, positive_post_count: rating.positive_post_count, negative_post_count: rating.negative_post_count, neutral_post_count: rating.neutral_post_count, mixed_post_count: rating.mixed_post_count, top_twenty_topic_words: []}
-      popular_words = []
-      school_words = SchoolWordCount.where(school_id: @school.id, topic_id: rating.topic.id).all
-      school_words.each do |word|
-        popular_words << {name: word.reference_word.name, count: word.word_count, positive_word_count: word.positive_word_count, negative_word_count: word.negative_word_count, mixed_word_count: word.mixed_word_count, neutral_word_count: word.neutral_word_count, multiplier: 30}
-      end
-      top_twenty = popular_words.sort{|least_popular,most_popular| most_popular[:count] <=> least_popular[:count]}
-      top_twenty.first(20).each do |popular_word_hash|
-        school_data["school_topic_posts_count"][index][:top_twenty_topic_words] << popular_word_hash
-      end
-      popular_words = []
-    end
-    @all_school_data = school_data["school_topic_posts_count"]
+    # @school.ratings.each_with_index do |rating, index|
+    #   school_data["school_topic_posts_count"] << {school: rating.school.name, name: rating.topic.name, count: rating.total_post_count, positive_post_count: rating.positive_post_count, negative_post_count: rating.negative_post_count, neutral_post_count: rating.neutral_post_count, mixed_post_count: rating.mixed_post_count, top_twenty_topic_words: []}
+    #   popular_words = []
+    #   school_words = SchoolWordCount.where(school_id: @school.id, topic_id: rating.topic.id).all
+    #   school_words.each do |word|
+    #     popular_words << {name: word.reference_word.name, count: word.word_count, positive_word_count: word.positive_word_count, negative_word_count: word.negative_word_count, mixed_word_count: word.mixed_word_count, neutral_word_count: word.neutral_word_count, multiplier: 30}
+    #   end
+    #   top_twenty = popular_words.sort{|least_popular,most_popular| most_popular[:count] <=> least_popular[:count]}
+    #   top_twenty.first(20).each do |popular_word_hash|
+    #     school_data["school_topic_posts_count"][index][:top_twenty_topic_words] << popular_word_hash
+    #   end
+    #   popular_words = []
+    # end
+    # @all_school_data = school_data["school_topic_posts_count"]
 
       if params[:school_id] == "1"
         render "_school_compare", :layout => false
@@ -98,55 +98,55 @@ class SchoolsController < ApplicationController
     p @ratings_profile.to_json
     #Post count and sentiment by topic and most popular words by topic for a particular school
     ##########################################
-    school_data = {"school_topic_posts_count" => []}
+    # school_data = {"school_topic_posts_count" => []}
 
-    school_sorted_ratings = @ratings.order("total_post_count desc")
-    # p school_sorted_ratings
-    @school.ratings.each_with_index do |rating, index|
-      # puts "------------------------------------------"
-      school_data["school_topic_posts_count"] << {school: rating.school.name, name: rating.topic.name, count: rating.total_post_count, positive_post_count: rating.positive_post_count, negative_post_count: rating.negative_post_count, neutral_post_count: rating.neutral_post_count, mixed_post_count: rating.mixed_post_count, top_twenty_topic_words: []}
-      # puts school_data
-      popular_words = []
-      # puts "======================================="
-      # puts rating.topic
-      # ++++++++++++++
-      school_words = SchoolWordCount.where(school_id: @school.id, topic_id: rating.topic.id).all
-      # p school_words
-      school_words.each do |word|
-        # p topic
-        # popular_word = SchoolWordCount.find_by_school_id_and_topic_id(@school.id, rating.topic.id)
-        # p popular_word
-        popular_words << {name: word.reference_word.name, count: word.word_count, positive_word_count: word.positive_word_count, negative_word_count: word.negative_word_count, mixed_word_count: word.mixed_word_count, neutral_word_count: word.neutral_word_count}
-      end
-      # +++++++++++++++
-      # reference_words = rating.topic.reference_words.all
-      # reference_words.each do |word|
-      #   # puts "%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
-      #   popular_word = SchoolWordCount.find_by_school_id_and_reference_word_id(@school.id, word.id)
-      #   if popular_word
-      #     popular_words << {name: popular_word.reference_word.name, count: popular_word.word_count, positive_word_count: popular_word.positive_word_count, negative_word_count: popular_word.negative_word_count, mixed_word_count: popular_word.mixed_word_count, neutral_word_count: popular_word.neutral_word_count, multiplier: 30}
-      #     # topics_count_holding["school_topic_posts_count"][index][:top_ten_topic_words] << {name: popular_word.reference_word.name, count: popular_word.word_count, positive_word_count: popular_word.positive_word_count, negative_word_count: popular_word.negative_word_count, mixed_word_count: popular_word.mixed_word_count, neutral_word_count: popular_word.neutral_word_count, multiplier: 30}
-      #   end
-      # end
-      # puts "---------------------------"
-      # p popular_words
-      top_twenty = popular_words.sort{|least_popular,most_popular| most_popular[:count] <=> least_popular[:count]}
-      top_twenty.first(20).each do |popular_word_hash|
-        school_data["school_topic_posts_count"][index][:top_twenty_topic_words] << popular_word_hash
-      end
-      popular_words = []
-    end
-    # p school_data
-    @all_school_data = school_data["school_topic_posts_count"]
-    # p @all_school_data
-      # rating.topic.reference_words.each do |reference_word|
-      #   top_ten_school_word_counts = []
-      #   SchoolWordCount.where(school_id: @school.id, reference_word_id: reference_word.id).order('word_count desc').limit(10)
-      #   p top_ten_school_word_counts
-      #   top_ten_school_word_counts.each do |keyword|
-      #     topics_count_holding["school_topic_posts_count"][index][:top_ten_topic_words] << {name: keyword.reference_word.name, word_count: keyword.word_count, positive_word_count: keyword.positive_word_count, negative_word_count: keyword.negative_word_count, mixed_word_count: keyword.mixed_word_count, neutral_word_count: keyword.neutral_word_count}
-      #   end
-    #####################################################This school's activity by topic
+    # school_sorted_ratings = @ratings.order("total_post_count desc")
+    # # p school_sorted_ratings
+    # @school.ratings.each_with_index do |rating, index|
+    #   # puts "------------------------------------------"
+    #   school_data["school_topic_posts_count"] << {school: rating.school.name, name: rating.topic.name, count: rating.total_post_count, positive_post_count: rating.positive_post_count, negative_post_count: rating.negative_post_count, neutral_post_count: rating.neutral_post_count, mixed_post_count: rating.mixed_post_count, top_twenty_topic_words: []}
+    #   # puts school_data
+    #   popular_words = []
+    #   # puts "======================================="
+    #   # puts rating.topic
+    #   # ++++++++++++++
+    #   school_words = SchoolWordCount.where(school_id: @school.id, topic_id: rating.topic.id).all
+    #   # p school_words
+    #   school_words.each do |word|
+    #     # p topic
+    #     # popular_word = SchoolWordCount.find_by_school_id_and_topic_id(@school.id, rating.topic.id)
+    #     # p popular_word
+    #     popular_words << {name: word.reference_word.name, count: word.word_count, positive_word_count: word.positive_word_count, negative_word_count: word.negative_word_count, mixed_word_count: word.mixed_word_count, neutral_word_count: word.neutral_word_count}
+    #   end
+    #   # +++++++++++++++
+    #   # reference_words = rating.topic.reference_words.all
+    #   # reference_words.each do |word|
+    #   #   # puts "%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
+    #   #   popular_word = SchoolWordCount.find_by_school_id_and_reference_word_id(@school.id, word.id)
+    #   #   if popular_word
+    #   #     popular_words << {name: popular_word.reference_word.name, count: popular_word.word_count, positive_word_count: popular_word.positive_word_count, negative_word_count: popular_word.negative_word_count, mixed_word_count: popular_word.mixed_word_count, neutral_word_count: popular_word.neutral_word_count, multiplier: 30}
+    #   #     # topics_count_holding["school_topic_posts_count"][index][:top_ten_topic_words] << {name: popular_word.reference_word.name, count: popular_word.word_count, positive_word_count: popular_word.positive_word_count, negative_word_count: popular_word.negative_word_count, mixed_word_count: popular_word.mixed_word_count, neutral_word_count: popular_word.neutral_word_count, multiplier: 30}
+    #   #   end
+    #   # end
+    #   # puts "---------------------------"
+    #   # p popular_words
+    #   top_twenty = popular_words.sort{|least_popular,most_popular| most_popular[:count] <=> least_popular[:count]}
+    #   top_twenty.first(20).each do |popular_word_hash|
+    #     school_data["school_topic_posts_count"][index][:top_twenty_topic_words] << popular_word_hash
+    #   end
+    #   popular_words = []
+    # end
+    # # p school_data
+    # @all_school_data = school_data["school_topic_posts_count"]
+    # # p @all_school_data
+    #   # rating.topic.reference_words.each do |reference_word|
+    #   #   top_ten_school_word_counts = []
+    #   #   SchoolWordCount.where(school_id: @school.id, reference_word_id: reference_word.id).order('word_count desc').limit(10)
+    #   #   p top_ten_school_word_counts
+    #   #   top_ten_school_word_counts.each do |keyword|
+    #   #     topics_count_holding["school_topic_posts_count"][index][:top_ten_topic_words] << {name: keyword.reference_word.name, word_count: keyword.word_count, positive_word_count: keyword.positive_word_count, negative_word_count: keyword.negative_word_count, mixed_word_count: keyword.mixed_word_count, neutral_word_count: keyword.neutral_word_count}
+    #   #   end
+    # #####################################################This school's activity by topic
   end
 
 end
